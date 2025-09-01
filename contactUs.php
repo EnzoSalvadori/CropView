5666d0bf457ee0e30fb34753066bd6c6bea793ac
EnzoSalvadori <63012963+EnzoSalvadori@users.noreply.github.com>
2025-09-01 15:35:44 -0300

    Update contactUs.php

--- contactUs.php
+++ contactUs.php
@@ -1,27 +1,4 @@
 <?php 
-if (!empty($_POST))
-{
-    $name = $_POST['name'];
-    $lastName = $_POST['lastName'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $assunto =  $_POST['assunto'];
-    $message = $_POST['message'];
-    $soma = $_POST['soma'];
-    $idioma = $_COOKIE["idioma"];
-
-    if ($soma == 11)
-    {
-      $formcontent=" From: $name $lastName \n E-mail: $email \n Phone: $phone \n Message: $message";
-      $recipient = "contato@cropview.com.br";
-      $subject = "CropView - Formulário de $assunto $soma";
-      $mailheader = "From: ... \r\n";
-      mail($recipient, $subject, $formcontent, $mailheader) or die("Erro ao enviar mensagem! Tente novamente ou entre em contato direto pelo e-mail contato@cropview.com.br!");
-      header("Location: /");
-      exit();
-    }
-
-}
 
 if (!empty($_GET['language'])){
     $idioma = $_GET['language'];
@@ -55,15 +32,5 @@ if (!empty($_GET['language'])){
   }else{
     include("contactUs1.html");
   }
-}
-
-if (!empty($_POST))
-{
- if ($soma != 11)
-    {
-       ?>
-        <script> document.getElementById('alert').style.display = '';</script>
-      <?php
-    }
 }
 ?>
