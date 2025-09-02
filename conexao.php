7b6fe62a25f12cbdfdc7443f919e022203b2df81
EnzoSalvadori <63012963+EnzoSalvadori@users.noreply.github.com>
2025-09-02 15:16:18 -0300

    cropview flask 1.0

--- conexao.php
+++
@@ -1,9 +0,0 @@
- <?php 
-
-require 'vendor/autoload.php';
-
-$client  = new MongoDB\Client("mongodb://localhost:27017");
-$dataBase = $client->selectDatabase('cropView');
-$collection = $dataBase->selectCollection("user");
-
-?>
