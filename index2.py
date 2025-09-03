f70f1f7373f261ff360d4a2ac805694fc8feeced
EnzoSalvadori <enzosalvadori17@gmail.com>
2025-09-02 21:22:35 -0300

    ajuste try if

--- index2.py
+++ index2.py
@@ -7,55 +7,55 @@ app.secret_key = "segredo"  # necessário para usar session
 @app.route("/inicio")
 @app.route("/home")
 def index():
-	if session["lang"]:
+	try:
 		if session["lang"] == "pt":
 			return render_template("index1.html")
 		if session["lang"] == "en":
 			return render_template("index2.html")
 		if session["lang"] == "es":
 			return render_template("index3.html")
-	else:
+	except:
 		return render_template("index1.html")
 
 @app.route("/requisitos")
 @app.route("/requirements")
 def requisitos():
-	if session["lang"]:
+	try:
 		if session["lang"] == "pt":
 			return render_template("requirements1.html")
 		if session["lang"] == "en":
 			return render_template("requirements2.html")
 		if session["lang"] == "es":
 			return render_template("requirements3.html")
-	else:
+	except:
 		return render_template("requirements1.html")
 
 @app.route("/precios")
 @app.route("/preços")
 @app.route("/pricing")
 def preços():
-	if session["lang"]:
+	try:
 		if session["lang"] == "pt":
 			return render_template("pricing1.html")
 		if session["lang"] == "en":
 			return render_template("pricing2.html")
 		if session["lang"] == "es":
 			return render_template("pricing3.html")
-	else:
+	except:
 		return render_template("pricing1.html")
 
 @app.route("/contato")
 @app.route("/contacto")
 @app.route("/contact")
 def contato():
-	if session["lang"]:
+	try:
 		if session["lang"] == "pt":
 			return render_template("contactUs1.html")
 		if session["lang"] == "en":
 			return render_template("contactUs2.html")
 		if session["lang"] == "es":
 			return render_template("contactUs3.html")
-	else:
+	except:
 		return render_template("contactUs1.html")
 
 @app.route("/set_language", methods=["POST"])
